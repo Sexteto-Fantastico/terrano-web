49dad140e17f7d93e64c77192a4247279230aeda
Pedro Zich <zichpedro22@gmail.com>
2026-04-04 22:44:09 -0300

    feat: add NotFound component and integrate it into routing

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,32 +1,9 @@
-import { Button } from "@/components/ui/button";
-import {
-  HeadContent,
-  Link,
-  Outlet,
-  createRootRoute,
-} from "@tanstack/react-router";
-import { ArrowLeft } from "lucide-react";
+import { HeadContent, Outlet, createRootRoute } from "@tanstack/react-router";
+import { NotFound } from "@/components/not-found";
 
 export const Route = createRootRoute({
   component: RootComponent,
-  notFoundComponent: () => {
-    return (
-      <div className="flex h-screen flex-col items-center justify-center gap-4">
-        <h1 className="text-3xl font-extrabold tracking-tight text-balance">
-          Página não encontrada
-        </h1>
-        <p className="text-lg text-muted-foreground">
-          A página que você está procurando não existe.
-        </p>
-        <Button variant={"link"} asChild>
-          <Link to="/">
-            <ArrowLeft className="size-4" />
-            Voltar para a página inicial
-          </Link>
-        </Button>
-      </div>
-    );
-  },
+  notFoundComponent: NotFound,
 });
 
 function RootComponent() {
